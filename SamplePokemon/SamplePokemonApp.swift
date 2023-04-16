99a8de28fef03437c45f140bae0464c813bd521d
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-04-16 12:26:10 +0900

    Initial Commit

---
+++ SamplePokemon/SamplePokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamplePokemonApp.swift
+//  SamplePokemon
+//
+//  Created by 渡邊魁優 on 2023/04/16.
+//
+
+import SwiftUI
+
+@main
+struct SamplePokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
